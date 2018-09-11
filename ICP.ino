cad82aad362787dc19f52d34055a003da4b8c0fb
Shoaib Ahmed Shaikh <30997997+Shoaib4AT@users.noreply.github.com>
2018-09-11 22:11:03 +0530

    ICP
    Arduino C++

---
+++ ICP.ino
@@ -0,0 +1,21 @@
+void setup() 
+{
+  pinMode(8,INPUT_PULLUP);
+  pinMode(13,OUTPUT);
+
+  TIMSK1 = (1<<ICIE1);
+  TCCR1B = (1<<ICNC1) | (1<<CS11);  //  Noise Canceler,1/8 Prescaler 
+  TCCR1B &=~ (1<<ICES1);            // Falling edge select
+  
+  sei();
+}
+
+void loop()
+{
+  
+
+}
+ISR(TIMER1_CAPT_vect)
+{      
+    PORTB ^= 1<<PB5;  
+}
